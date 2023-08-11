b640514b98516a7c256cebcdfb82f751629b665d
Vasily <vyurevich@apro-software.com>
2023-08-11 09:43:39 +0300

    add changes 5.0

--- front/components/SwapReward.js
+++ front/components/SwapReward.js
@@ -123,7 +123,7 @@ export function SwapReward({_active}) {
                                        	onClick={ ()=> document.getElementById('connect-wallet').click()}
                                         className="bg-gradient-to-r from-[#29C8A9] to-[#703AAD] text-textColor font-medium rounded-md sx:max-w-[157px] max-h-[50px] w-[200px] md:px-5 px-4 py-3 sm:text-[16px] text-[18px] sx:py-[12px] sx:px-[16px] transform-gpu transition-transform duration-200 ease-in-out hover:scale-95 focus:scale-95 active:scale-95 "
 
-                                    /> :
+                                    >Connect Wallet</button> :
                                     selectAccount &&
                                     <div
                                         onClick={disconnect}
