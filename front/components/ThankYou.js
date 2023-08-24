a38bea497b069853fdf8527adb1b5d7e038740fa
IharDorinel <ihardaronin@gmail.com>
2023-08-24 13:36:45 +0300

    fix font color

--- front/components/ThankYou.js
+++ front/components/ThankYou.js
@@ -33,7 +33,7 @@ export function ThankYou({getMultiplier, handleSetActive, getConfirmationComplet
                         <p className="text-3xl font-medium text-center">Thank you!</p>
                         <p className="text-base font-normal text-center">Amet minim mollit non deserunt ullamco est sit aliqua.
                         { !getIsUserUseMultiplayer  &&
-                            <span className="block">
+                            <span className="block text-primaryBgColor">
                                Get your 90% discount with x{getMultiplier>0 && getMultiplier} multiplier!
                            </span>
                         }
